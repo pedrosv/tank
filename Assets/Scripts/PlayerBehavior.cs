80e43ff912e3e3904fc75f931308a69296b617c7
Pedro S <pedrosv6@gmail.com>
2018-04-23 03:50:28 -0300

    resolvendo bug de destruir parede tocando nela

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -24,7 +24,7 @@ public class PlayerBehavior : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemies")
+        if (collision.gameObject.tag == "Enemies") { 
             health--;
             Destroy(collision.gameObject);
         }
