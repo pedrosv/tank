8c8c6912aef94ee222d20712dda340dfc330da25
Pedro S <pedrosv6@gmail.com>
2018-04-23 04:15:44 -0300

    mostrando score

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public Text scoreText;
+
+    // Use this for initialization
+    void Start () {
         Cursor.visible = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        showScore();
+    }
+
+    private void showScore()
+    {
+        scoreText.text = "Score: " + PlayerStats.Kills;
+    }
 }
